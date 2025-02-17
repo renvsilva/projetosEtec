404890c863f661566aeb8e6e957a46c4b5ca1dd5
luscapelli <alcantaraminacapellilucas@gmail.com>
2025-02-17 10:33:25 -0300

    Added image and basic CSS

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>B  R  A  S  I  L!</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={require("./assets/bandeiragrande.jpg")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,7 +24,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: "#FFFFFF",
-    backgroundColor: "#000000",
+    color: "#ffcb00",
+    backgroundColor: "#009440",
+  },
+  tinyLogo: {
+    width: 400,
+    height: 200,
+  },
+  logo: {
+    width: 66,
+    height: 58,
   },
 });
